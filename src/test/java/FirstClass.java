9879776ceb357bd3731d708213b731c7e3ba29e1
ZakiSF <58540617+ZakiSF@users.noreply.github.com>
2021-06-09 19:58:15 -0700

    Update FirstClass.java
    Attract added

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,7 +1,7 @@
 public class FirstClass {
     public static void main(String[] args) {
 
-        System.out.println("hello success");
+        System.out.println("hello success is somthing you attract by the person you become");
 
 
     }
