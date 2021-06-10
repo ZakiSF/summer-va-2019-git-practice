ccdeb7c70de83495fb711df116f60b7bd464cc71
Sayedgit4 <yasirmiri17@gmail.com>
2021-06-09 20:02:59 -0700

    2 added

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -1,6 +1,6 @@
 public class SecondClass {
 
     public static void main(String[] args) {
-        System.out.println("Success come by the person you become");
+        System.out.println("Success added2");
     }
 }
